5fe68e3fac78d56fd08156944a48d19427ab0c94
chris <chris@bumblehead.com>
2018-03-30 02:37:45 -0700

    initial commit

---
+++ replace-imports.js
@@ -0,0 +1,20 @@
+// Filename: replace-imports.js  
+// Timestamp: 2018.03.30-00:51:52 (last modified)
+// Author(s): bumblehead <chris@bumblehead.com>  
+
+const acorn = require('acorn');
+
+module.exports = (content, replacements) => acorn.parse(content, {
+  sourceType: 'module',
+  allowImportExportEverywhere: true
+}).body.reduce((content, spec) => {
+  if (spec.type === 'ImportDeclaration' &&
+      spec.source.value in replacements) {
+    content =
+      content.substring(0, ++spec.source.start) +
+      replacements[spec.source.value] +
+      content.substring(--spec.source.end);
+  }
+
+  return content;
+}, content);
---
+++ spec/replace-imports.spec.js
@@ -0,0 +1,8 @@
+let replaceimports = require('../');
+
+describe("replaceimports", () => {
+  it("should return true", () =>
+    expect(
+      true
+    ).toBe(true));
+});
